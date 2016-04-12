28629b88a8f1864f732a0521410178c4b6f4046d
Gurwinder Singh <gurwinder.61ds@gmail.com>
2016-04-12 17:58:46 +0530

    Master commit

---
+++ _protected/common/config/main-local.php
@@ -0,0 +1,30 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=clos_dev',
+            'username' => 'clos_dev',
+            'password' => 'Dev@1234',
+            'charset' => 'utf8',
+			
+			//comment below for disable cache
+/* 			'enableSchemaCache' => true,
+			// Duration of schema cache.
+			'schemaCacheDuration' => 3600,
+
+			// Name of the cache component used to store schema information
+			'schemaCache' => 'cache',	 */		
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
+
+
